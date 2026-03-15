df3edc3f904269aaa635bda40b427ee388d1586f
thekarstens <weatherworkshops@gmail.com>
2026-03-14 19:48:44 -0500

    Add files via upload

--- engine.js
+++ engine.js
@@ -3144,13 +3144,12 @@ function updateAlerts(){
   chips.forEach(function(chip){
     var action = chip.getAttribute('data-action');
     if (action === 'satellite'){
-      var lessonCfg = (window.__LESSON_CFG__ || window.CFG || {});
-      var satOn = !!(lessonCfg && lessonCfg.satellite && lessonCfg.satellite.enabled);
-      if (satOn){
-        chip.disabled = false;
-        chip.removeAttribute('disabled');
-        chip.classList.remove('disabled');
-      }
+      chip.disabled = false;
+      chip.removeAttribute('disabled');
+      chip.classList.remove('disabled');
+      chip.style.pointerEvents = 'auto';
+      chip.style.opacity = '1';
+      chip.style.filter = 'none';
     }
     chip.addEventListener('click', async function(ev){
       ev.preventDefault();
